111448044687193ea27954ef69c0689c7e3558bf
ketan-1993 <54744772+ketan-1993@users.noreply.github.com>
2021-10-31 16:32:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "tata111"
-    key    = "tata123/project123"
+    key    = "tata111/project"
     region = "ap-south-1"
     access_key = "AKIA4QIWBQQQJ53AR3UX"
     secret_key = "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"
