4067805824c981060babfff017dab2cfd3b38fa5
ketan-1993 <54744772+ketan-1993@users.noreply.github.com>
2021-10-31 15:48:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "ap-south-1"
   access_key = "AKIA4QIWBQQQJ53AR3UX"
   secret_key = "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"
+}
+terraform {
+  backend "s3" {
+    bucket = "tatatatata"
+    key    = "tatatatata/project"
+    region = "ap-south-1"
+    access_key = "AKIA4QIWBQQQJ53AR3UX"
+    secret_key = "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"
+}
+    
+  }
 }
 resource "aws_vpc" "main" {
   count            = 2
