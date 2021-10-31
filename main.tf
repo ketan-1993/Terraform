c8bd1f9ee3a643438b625745a4e848cf5bb56c5a
ketan-1993 <54744772+ketan-1993@users.noreply.github.com>
2021-10-31 13:48:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA4QIWBQQQJ53AR3UX"
+  secret_key = "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"
+}
 resource "aws_vpc" "main" {
   count            = 2
   cidr_block       = "10.0.0.0/16"
